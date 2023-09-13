70b8255ad98ae8a13a46a9aff3594e93d06b3de3
iliaant <144952944+iliaant@users.noreply.github.com>
2023-09-13 22:21:10 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int counting(string name, int var)
+{
+	char name_1 = name[0];
+	int name_int = (int)name_1;
+	return name_int % var;
+}
+
+int main(){
+	int n, i;
+	string name;
+	cin >> name;
+	cin >> n;
+	cout << counting(name, n);
+}
